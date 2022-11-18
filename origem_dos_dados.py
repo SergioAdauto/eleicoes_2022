7c8d468a639a1c070fe458ef0e150aca6f824bb2
SergioAdauto <sergioadauto@hotmail.com>
2022-11-18 18:10:46 +0000

    arquivo python que baixa os dados do tse e separa em arquivos menosres

---
+++ origem_dos_dados.py
@@ -0,0 +1,14 @@
+import pandas as pd
+#Dados da base do TSE:
+dados_tse = "https://cdn.tse.jus.br/estatistica/sead/odsele/arquivos_gerados/resultados/20221115/682c812ccb32e42937205141b45d9fcf/votacao_candidato.csv.zip"
+df = pd.read_csv(filepath_or_buffer=dados_tse,  sep=';', encoding='latin1')
+
+#Array dos estados basileiros:
+uf = df.sg_uf.unique()
+
+#Loop para separar as informações por estado e gravar os dados em CSV:
+for i in uf:
+  df_uf = df.query(f'sg_uf == "{i}"')
+  df_uf.to_csv(path_or_buf=f'/content/eleicoes_2022/dados/{i}.csv',sep=';',index=False)
+
+  
